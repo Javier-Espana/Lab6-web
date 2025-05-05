2a2017471146f438f48305003382e7acea545782
Javier-Espana <javier.eduardo.espana@gmail.com>
2025-05-05 02:38:29 -0600

    Docs update

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -19,12 +19,12 @@ const docTemplate = `{
 
 // SwaggerInfo contiene la información exportada de Swagger para que los clientes puedan modificarla
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
-	Host:             "",
-	BasePath:         "",
-	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
+	Version:          "1.0.0",                                                    // Versión de la API
+	Host:             "localhost:8080",                                           // Host donde se ejecuta la API
+	BasePath:         "/",                                                        // BasePath de la API
+	Schemes:          []string{"http"},                                           // Esquema utilizado (http o https)
+	Title:            "Documentación de la API",                                  // Título de la documentación
+	Description:      "Esta es la documentación de la API generada con Swagger.", // Descripción de la API
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
